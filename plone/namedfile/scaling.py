1d559fc4326d81eae7830785e45c3d71d2d6f29a
Gil Forcada <gil.gnome@gmail.com>
2018-02-27 14:19:09 +0100

    Add overlay for daily comment articles

--- plone/namedfile/scaling.py
+++ plone/namedfile/scaling.py
@@ -541,6 +541,9 @@ def add_overlay_image(orig_data, contentType, width, context):
     if _is_recommended(context):
         return _add_overlay(orig_data, contentType, width, 'Recommended')
 
+    elif _is_daily_comment(context):
+        return _add_overlay(orig_data, contentType, width, 'Daily')
+
     return orig_data
 
 
@@ -584,3 +587,14 @@ def _add_overlay(orig_data, contentType, width, overlay_type):
 def _is_recommended(context):
     from plone import api
     return api.content.get_state(context) == 'recommended'
+
+
+def _is_daily_comment(context):
+    from plone import api
+    if api.content.get_state(context) != 'published':
+        return False
+
+    if 'kommentar des tages' in (x.lower() for x in context.Subject()):
+        return True
+
+    return False
